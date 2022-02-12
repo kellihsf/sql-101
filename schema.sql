bb11ea46f7a3df9794f3db222f98aa56b8d39db7
Kelli <kellistone217@gmail.com>
2022-02-12 11:40:56 -0700

    Added initial files

---
+++ schema.sql
@@ -0,0 +1,22 @@
+create table users (
+    id serial primary key,
+    name varchar(50) not null,
+    email varchar(50) not null
+);
+
+create table posts (
+    id serial primary key,
+    url text not null,
+    user_id integer references users (id)
+    -- Foreign Key
+);
+
+create table tags (
+    id serial primary key,
+    tag varchar(50) not null
+);
+
+create table tags_posts (
+    tag_id integer references tags (id),
+    post_id integer references posts (id)
+);
